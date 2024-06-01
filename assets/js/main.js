d9c6634c9f75674132ebd3620669c98209a48e2b
radoheritiana <radoheritiana.rhja.123@gmail.com>
2024-06-01 17:14:10 +0300

    add state page

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,10 +11,34 @@ window.onload = function() {
 
     window.addEventListener('scroll', function() {
 
+        console.log(this.window.pageYOffset);
+
         if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
             document.getElementById("header").classList.add('scroll');
         } else {
             document.getElementById("header").classList.remove('scroll');
+        }
+
+        if(window.pageYOffset > 0 && window.pageYOffset < 350) {
+            document.getElementById('state0').classList.add("rect-active");
+            document.getElementById('state1').classList.remove("rect-active");
+            document.getElementById('state2').classList.remove("rect-active");
+            document.getElementById('state3').classList.remove("rect-active");
+        } else if(window.pageYOffset > 350 && window.pageYOffset < 700) {
+            document.getElementById('state1').classList.add("rect-active");
+            document.getElementById('state0').classList.remove("rect-active");
+            document.getElementById('state2').classList.remove("rect-active");
+            document.getElementById('state3').classList.remove("rect-active");
+        } else if (window.pageYOffset > 700 && window.pageYOffset < 1200) {
+            document.getElementById('state2').classList.add("rect-active");
+            document.getElementById('state1').classList.remove("rect-active");
+            document.getElementById('state0').classList.remove("rect-active");
+            document.getElementById('state3').classList.remove("rect-active");
+        } else if (window.pageYOffset > 1200) {
+            document.getElementById('state3').classList.add("rect-active");
+            document.getElementById('state1').classList.remove("rect-active");
+            document.getElementById('state2').classList.remove("rect-active");
+            document.getElementById('state0').classList.remove("rect-active");
         }
 
         let scroll = window.pageYOffset;
