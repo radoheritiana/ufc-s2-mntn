e2bd72864c6c9b956d12f278a5cb440f69ab560d
radoheritiana <radoheritiana.rhja.123@gmail.com>
2024-06-01 14:34:15 +0300

    add loading and animation on scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,22 @@
 
 
 window.onload = function() {
+
+    setTimeout(function(){
+        document.getElementById('loading').classList.add("hidden");
+        document.getElementById('main').classList.remove("hidden");
+    }, 3000);
+
     const translate = document.querySelectorAll(".translate");
 
     window.addEventListener('scroll', function() {
+
+        if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+            document.getElementById("header").classList.add('scroll');
+        } else {
+            document.getElementById("header").classList.remove('scroll');
+        }
+
         let scroll = window.pageYOffset;
 
         translate.forEach(element => {
